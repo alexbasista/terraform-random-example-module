18dfbfd157b23ed046ecfe89c02cdc79174fe5a2
alexbasista <abasista@hashicorp.com>
2025-04-15 16:39:02 +0900

    add more pets

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "random_pet_id" {
-  value       = random_pet.example.id
+output "random_pet_id_1" {
+  value       = random_pet.example_1.id
+  description = "ID (name) of the random pet."
+}
+
+output "random_pet_id_2" {
+  value       = random_pet.example_2.id
+  description = "ID (name) of the random pet."
+}
+
+output "random_pet_id_3" {
+  value       = random_pet.example_3.id
   description = "ID (name) of the random pet."
 }
