37b5ef6e23447a10193319b2dfef02d5f58b5e80
alexbasista <abasista@hashicorp.com>
2025-04-16 09:42:01 +0900

    update test config

--- tests/default/outputs.tf
+++ tests/default/outputs.tf
@@ -1,4 +1,19 @@
-output "random_pet_id" {
-  value       = module.example_module.random_pet_id
+output "random_pet_id_1" {
+  value       = module.example_module.random_pet_id_1
+  description = "ID (name) of random pet created by the example module."
+}
+
+output "random_pet_id_2" {
+  value       = module.example_module.random_pet_id_2
+  description = "ID (name) of random pet created by the example module."
+}
+
+output "random_pet_id_3" {
+  value       = module.example_module.random_pet_id_3
+  description = "ID (name) of random pet created by the example module."
+}
+
+output "random_pet_id_4" {
+  value       = module.example_module.random_pet_id_4
   description = "ID (name) of random pet created by the example module."
 }
