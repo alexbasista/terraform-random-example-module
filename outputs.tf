fc6100a5d0d5ccdc89ca28381620c3e24615db8f
Alex Basista <abasista@hashicorp.com>
2025-08-12 10:48:19 -0400

    add 3rd pet

--- outputs.tf
+++ outputs.tf
@@ -8,10 +8,10 @@ output "random_pet_id_2" {
   description = "ID (name) of the random pet 2."
 }
 
-# output "random_pet_id_3" {
-#   value       = random_pet.example_3.id
-#   description = "ID (name) of the random pet 3."
-# }
+output "random_pet_id_3" {
+  value       = random_pet.example_3.id
+  description = "ID (name) of the random pet 3."
+}
 
 # output "random_pet_id_4" {
 #   value       = random_pet.example_4.id
