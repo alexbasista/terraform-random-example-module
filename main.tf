c45f4c5696815d3c83369f20514237d08eab1853
Alex Basista <abasista@hashicorp.com>
2025-08-13 10:37:13 -0400

    add 3rd pet (#9)

--- main.tf
+++ main.tf
@@ -18,15 +18,15 @@ resource "random_pet" "example_2" {
   # }
 }
 
-# resource "random_pet" "example_3" {
-#   length    = var.pet_length
-#   prefix    = var.pet_prefix
-#   separator = var.pet_separator
+resource "random_pet" "example_3" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  #separator = var.pet_separator
 
-#   keepers = {
-#     trigger_change = var.trigger_change
-#   }
-# }
+  # keepers = {
+  #   trigger_change = var.trigger_change
+  # }
+}
 
 # resource "random_pet" "example_4" {
 #   length    = var.pet_length
