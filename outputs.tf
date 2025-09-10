f9348a1fa26f78cadffd6d66bd228bc0a43da627
Alex Basista <abasista@hashicorp.com>
2025-09-10 09:22:09 -0400

    normalize 1.0.0

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@ output "random_pet_id_1" {
   description = "ID (name) of the random pet 1."
 }
 
-output "random_pet_id_2" {
-  value       = random_pet.example_2.id
-  description = "ID (name) of the random pet 2."
-}
+# output "random_pet_id_2" {
+#   value       = random_pet.example_2.id
+#   description = "ID (name) of the random pet 2."
+# }
 
 # output "random_pet_id_3" {
 #   value       = random_pet.example_3.id
