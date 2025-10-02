120b6799c06cb257aec8f791df0a3f95d556a782
Alex Basista <abasista@hashicorp.com>
2025-10-02 11:42:54 -0400

    init

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "random_pet" "example_1" {
   prefix = var.pet_prefix
 }
 
-# resource "random_pet" "example_2" {
-#   length = var.pet_length
-#   prefix = var.pet_prefix
-# }
+resource "random_pet" "example_2" {
+  length = var.pet_length
+  prefix = var.pet_prefix
+}
 
 # resource "random_pet" "example_3" {
 #   length    = var.pet_length
--- tests/integration/outputs.tf
+++ tests/integration/outputs.tf
@@ -3,10 +3,10 @@ output "random_pet_id_1" {
   description = "ID (name) of random pet created by the example module."
 }
 
-# output "random_pet_id_2" {
-#   value       = module.example_module.random_pet_id_2
-#   description = "ID (name) of random pet created by the example module."
-# }
+output "random_pet_id_2" {
+  value       = module.example_module.random_pet_id_2
+  description = "ID (name) of random pet created by the example module."
+}
 
 # output "random_pet_id_3" {
 #   value       = module.example_module.random_pet_id_3
