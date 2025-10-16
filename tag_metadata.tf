eaf29734d587b9c33672fb8a81f248ff4de6eb2e
Alex Basista <abasista@hashicorp.com>
2025-10-16 13:17:15 -0400

    update

--- tag_metadata.tf
+++ tag_metadata.tf
@@ -1,10 +1,10 @@
-module "tags_metadata" {
+module "module_metadata_tags" {
   source  = "app.terraform.io/abasista-tfc/module-metadata-tags/random"
   version = "3.2.0"
   
   caller_module_path = path.module
 }
 
-output "debug_module_metadata" {
+output "debug_module_metadata_tags" {
     value = module.tags_metadata.debug
 }
